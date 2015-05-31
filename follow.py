b14b5c6debf5ca647100d6a32927771e2afd61ec
Sasha Bayan <SashaBayan@gmail.com>
2015-05-31 11:43:24 -0400

    sets up boiler-blate follow feature

---
+++ follow.py
@@ -0,0 +1,6 @@
+from twitter_follow_bot import auto_follow
+
+auto_follow("#js", count=10)
+
+#Phrases for auto-following/ favoriting
+# js, javascript, ecmascript, es6, esnext, angular, react.js, backbone.js, 
