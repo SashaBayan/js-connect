10a3004a0af74ba9827334d5acbdaed0f48760fa
Sasha Bayan <SashaBayan@gmail.com>
2015-06-07 20:15:06 -0700

    adds auto-fav function

--- follow.py
+++ follow.py
@@ -1,6 +1,7 @@
 from twitter_follow_bot import auto_follow
 
 auto_follow("#js", count=10)
+auto_fav("react.js", count=10)
 
-#Phrases for auto-following/ favoriting
+#Phrases for following/ favoriting/ RTs
 # js, javascript, ecmascript, es6, esnext, angular, react.js, backbone.js, 
